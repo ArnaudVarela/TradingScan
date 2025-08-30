a93ea26d546a8b370a518744af4dd6677921adbf
ArnaudVarela <75020810+ArnaudVarela@users.noreply.github.com>
2025-08-30 22:37:53 +0200

    Update tailwind.config.js

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "media", // ← auto: follows user's OS theme
+  darkMode: "class", // ← auto: follows user's OS theme
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: { extend: {} },
   plugins: [],
